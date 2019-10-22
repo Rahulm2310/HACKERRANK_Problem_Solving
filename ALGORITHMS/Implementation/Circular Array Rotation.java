5f85ad8ebda3473245f755608a2b47b46a0268da
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-10-22 18:44:50 +0530

    Create Circular Array Rotation.java

--- ALGORITHMS/Implementation/Circular Array Rotation.java
+++ ALGORITHMS/Implementation/Circular Array Rotation.java
@@ -1,34 +1,110 @@
 import java.io.*;
-import java.util.*;
-import java.text.*;
 import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt()%n;//We MOD k here because every n rotations we receive the same array
-        int q = in.nextInt();
-        int[] a = new int[n];
-        for(int a_i=0; a_i < n; a_i++){
-            a[a_i] = in.nextInt();
+    
+    static int[] circularArrayRotation(int[] a, int k, int[] queries)
+    {
+
+        int temp;
+        
+        if(a.length!=k)
+        {
+            
+            for(int i=0;i<k;i++)
+            {
+                temp=a[a.length-1];
+                
+                    for(int j=(a.length)-1;j>=1;j--)
+                    {
+                        a[j]=a[j-1];
+
+                    }
+                a[0]=temp;
+            }
         }
         
-        
-        //Run each query q
-        for(int a0 = 0; a0 < q; a0++){
-            int m = in.nextInt();
-            if(m-k >=0)//Checks if a array wrap occurs
-            {
-                System.out.println(a[m-k]);
-            }
-            else//if and array rap occurs we account for it
-            {
-                System.out.println(a[n+(m-k)]);
-            }
+
+        for(int l=0;l<queries.length;l++)
+        {
+            
+            queries[l]=a[queries[l]];
             
         }
-    } 
+
+        return queries;
+
+    }
+    
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException
+    {
+        
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nkq = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nkq[0]);
+
+        int k = Integer.parseInt(nkq[1]);
+
+        int q = Integer.parseInt(nkq[2]);
+
+        int[] a = new int[n];
+
+        
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        
+        for (int i = 0; i < n; i++) 
+        {
+            
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+            
+        }
+
+        int[] queries = new int[q];
+
+        for (int i = 0; i < q; i++) 
+        {
+            
+            int queriesItem = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+            queries[i] = queriesItem;
+            
+        }
+
+        int[] result = circularArrayRotation(a, k, queries);
+        
+
+        for (int i = 0; i < result.length; i++)
+        {
+            
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1)
+            {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+        
+        
+    }
 }
