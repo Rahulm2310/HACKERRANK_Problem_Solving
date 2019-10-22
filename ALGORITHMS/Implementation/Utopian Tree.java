3decdf09b1f0ac371f670e05a1d345b80067d628
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-10-22 18:35:45 +0530

    Create Utopian Tree.java

--- ALGORITHMS/Implementation/Utopian Tree.java
+++ ALGORITHMS/Implementation/Utopian Tree.java
@@ -1,27 +1,75 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
-import java.lang.Math;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        int testCases = input.nextInt();
+    
+    static int utopianTree(int n) 
+    {
+        int cycles=1;
+        int height=1;
         
-        for(int i = 0; i < testCases; i++)
+        while(cycles<=n)
         {
-            int cycles = input.nextInt();
             
-            //Calculates according the an equation defined by the arithmetico-geometric sequence
-            if(cycles % 2 == 0)
+            if(cycles%2!=0)
             {
-                System.out.println((int) (Math.pow(2, cycles/2)*2) -1);
+                
+                height=height*2;
+                
             }
-            else
+
+            else if(cycles%2==0)
             {
-                System.out.println((int) ((Math.pow(2, (cycles-1)/2)*2) -1)*2);
+                
+                height++;
+                
             }
+            
+            cycles++;
         }
+
+        return height;
+
+
+    }
+    
+    
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException
+    {
+        
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int result = utopianTree(n);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+            
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+        
+        
     }
 }
