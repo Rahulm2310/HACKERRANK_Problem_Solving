0d7bf394bba0d3056990665777fc0042aa021b37
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-10-22 18:38:43 +0530

    Create Angry Professor.java

--- ALGORITHMS/Implementation/Angry Professor.java
+++ ALGORITHMS/Implementation/Angry Professor.java
@@ -1,35 +1,83 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        int T = input.nextInt();
-        
-        testCases: 
-        for(int i = 0; i < T; i++)
+    // Complete the angryProfessor function below.
+    static String angryProfessor(int k, int[] a) {
+        int present=0;
+        String s=new String("");
+        for(int i=0;i<a.length;i++)
         {
-            int n = input.nextInt();
-            int k = input.nextInt();
-            int onTime = 0;
-            for(int j = 0; j < n; j++)
+            if(a[i]<=0)
             {
-                int arrivalTime = input.nextInt();
-                if(arrivalTime <= 0)
-                {
-                    onTime++;
-                }
-            }
-            if(onTime >= k)
-            {
-                System.out.println("NO");
-            }
-            else
-            {
-                System.out.println("YES");   
+                present++;
             }
         }
+
+        if(present>=k)
+        {
+            s="NO";
+        }
+        else{
+            s="YES";
+        }
+
+        return s;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException
+    
+    {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            
+            String[] nk = scanner.nextLine().split(" ");
+
+            int n = Integer.parseInt(nk[0]);
+
+            int k = Integer.parseInt(nk[1]);
+
+            int[] a = new int[n];
+            
+
+            String[] aItems = scanner.nextLine().split(" ");
+            
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            
+            for (int i = 0; i < n; i++) 
+            {
+                
+                int aItem = Integer.parseInt(aItems[i]);
+                a[i] = aItem;
+                
+            }
+
+            String result = angryProfessor(k, a);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+            
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+        
+        
     }
 }
