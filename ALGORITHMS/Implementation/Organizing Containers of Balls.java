aaf385703566d64b3fd40afce89397ec47e3d276
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-02 23:56:44 +0530

    Update Organizing Containers of Balls.java

--- ALGORITHMS/Implementation/Organizing Containers of Balls.java
+++ ALGORITHMS/Implementation/Organizing Containers of Balls.java
@@ -9,7 +9,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int q = in.nextInt();
-        for(int a0 = 0; a0 < q; a0++){
+        for(int a0 = 0; a0 < q; a0++)
+        {
             
             ///////////BUILD THE INPUT MATRIX///////////
             int n = in.nextInt();
