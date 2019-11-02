d118510855b79f6b34eac074ed400055d809130d
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-02 23:54:08 +0530

    Update Cut the Sticks.java

--- ALGORITHMS/Implementation/Cut the Sticks.java
+++ ALGORITHMS/Implementation/Cut the Sticks.java
@@ -25,6 +25,7 @@ public class Solution {
                     evenlyDivisibleDigits++;
                 }
             }
+            
             System.out.println(evenlyDivisibleDigits);
         }
     }
