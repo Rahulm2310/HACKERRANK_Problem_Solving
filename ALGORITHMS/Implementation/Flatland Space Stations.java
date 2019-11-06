e6838e0d30b1aa6356f090a2af6176ab3d474bd5
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-06 19:06:37 +0530

    Update Flatland Space Stations.java

--- ALGORITHMS/Implementation/Flatland Space Stations.java
+++ ALGORITHMS/Implementation/Flatland Space Stations.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner input = new Scanner(System.in);
         int n = input.nextInt(); //Number of cities
