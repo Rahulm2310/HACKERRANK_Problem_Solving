2bfdd6a3be769029054d9d173b90aea9eb400323
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-08 22:03:45 +0530

    Update Append and Delete.java

--- ALGORITHMS/Implementation/Append and Delete.java
+++ ALGORITHMS/Implementation/Append and Delete.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    public static void main(String[] args) 
+    {
         Scanner input = new Scanner(System.in);
         int T = input.nextInt();
         
