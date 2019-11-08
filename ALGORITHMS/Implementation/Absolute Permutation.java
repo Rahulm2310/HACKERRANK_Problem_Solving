88093c4e54839f168657882eec41428c140b6a1c
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-08 22:02:43 +0530

    Update Absolute Permutation.java

--- ALGORITHMS/Implementation/Absolute Permutation.java
+++ ALGORITHMS/Implementation/Absolute Permutation.java
@@ -6,7 +6,9 @@ import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         tests :
