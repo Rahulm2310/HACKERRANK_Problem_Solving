2d8d7a318bee927e711346e2b688e709a3236036
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-08 22:05:37 +0530

    Update Chocolate Feast.java

--- ALGORITHMS/Implementation/Chocolate Feast.java
+++ ALGORITHMS/Implementation/Chocolate Feast.java
@@ -4,8 +4,9 @@ import java.util.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    public static void main(String[] args)
+    {
+        
         Scanner input = new Scanner(System.in);
         int t = input.nextInt();
         
