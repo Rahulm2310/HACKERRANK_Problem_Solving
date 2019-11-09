c98adfbf1f90726b1947748b02521ed2a7227145
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-09 23:09:09 +0530

    Update Encryption.java

--- ALGORITHMS/Implementation/Encryption.java
+++ ALGORITHMS/Implementation/Encryption.java
@@ -4,7 +4,8 @@ import java.lang.Math;
 
 public class Solution {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner input = new Scanner(System.in);
         
