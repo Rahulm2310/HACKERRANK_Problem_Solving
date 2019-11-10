3046a3bc1af95cf48acf50f49195637da0bfe74e
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-10 23:45:25 +0530

    Update ACM ICPC Team.java

--- ALGORITHMS/Implementation/ACM ICPC Team.java
+++ ALGORITHMS/Implementation/ACM ICPC Team.java
@@ -1,52 +1,80 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
-import java.lang.Math;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int m = input.nextInt();
-        
-        String[] skills = new String[n];
-        int maxSkills = 0;
-        int maxSkillTeams = 0;
-        input.nextLine(); //advances the past  the first line of input  integers
-        
-        for(int i = 0; i < n; i++)
+    
+    static int[] acmTeam(String[] topic) 
+    {
+        int subject=0;
+        int maxsubject=0;
+        int teams=0;
+        int arr[]=new int[2];
+        for(int i=0;i<topic.length;i++)
         {
-            skills[i] = input.nextLine();
-        }
-        
-        
-        for(int i = 0; i < n; i++)
-        {
-            for(int j = i+1; j < n; j++)
-            {
-                String member1 = skills[i];
-                String member2 = skills[j];
-                int skillSet = 0;
-                
-                for(int k = 0; k < m; k++)
+            for(int j=i+1;j<topic.length;j++)
+            {  subject=0;
+                for(int k=0;k<topic[0].length();k++)
                 {
-                    if(member1.charAt(k) == '1' || member2.charAt(k) == '1')
+                    if(topic[i].charAt(k)=='1'||topic[j].charAt(k)=='1')
                     {
-                        skillSet++;
+                        subject++;
                     }
                 }
-
-                maxSkillTeams += (maxSkills == skillSet) ? 1 : 0;
-                
-                if(skillSet > maxSkills)
+                if(maxsubject<subject)
                 {
-                    maxSkillTeams = 1;
-                    maxSkills = skillSet;
+                    teams=1;
+                    maxsubject=subject;
+                }
+               else if(maxsubject==subject)
+                {
+                    teams++;
                 }
             }
         }
-        
-        System.out.println(maxSkills + "\n" + maxSkillTeams);
+        arr[0]=maxsubject;
+        arr[1]=teams;
+        return arr;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nm = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nm[0]);
+
+        int m = Integer.parseInt(nm[1]);
+
+        String[] topic = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            String topicItem = scanner.nextLine();
+            topic[i] = topicItem;
+        }
+
+        int[] result = acmTeam(topic);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write("\n");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
