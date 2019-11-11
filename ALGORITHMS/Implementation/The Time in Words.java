da8a801bdfc4353a335a98a9805217507df0ed7c
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-11 17:06:02 +0530

    Update The Time in Words.java

--- ALGORITHMS/Implementation/The Time in Words.java
+++ ALGORITHMS/Implementation/The Time in Words.java
@@ -1,60 +1,91 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        int H = input.nextInt();
-        int M = input.nextInt();
-        
-        HashMap<Integer, String> words = new HashMap<>();
-    
-        words.put(1,"one");words.put(2,"two");
-        words.put(3,"three");words.put(4,"four");
-        words.put(5,"five");words.put(6,"six");
-        words.put(7,"seven");words.put(8,"eight");
-        words.put(9,"nine");words.put(10,"ten");
-        words.put(11,"eleven");words.put(12,"twelve");
-        
-        words.put(13,"thirteen");
-        words.put(14,"fourteen");
-        words.put(15,"fifteen");
-        words.put(16,"sixteen");
-        words.put(17,"seventeen");
-        words.put(18,"eighteen");
-        words.put(19,"nineteen");
-        
-        ///////////////////////////////////////////////////////
-        
-        if (M == 0){System.out.print(words.get(H)+" o' clock");System.exit(0);}
-        
-        if(M < 30)
+   
+    static String timeInWords(int h, int m) {
+        String s=new String("");
+
+        String nums[] = {  "zero","one", "two", "three", "four", 
+                            "five", "six", "seven", "eight", "nine", 
+                            "ten", "eleven", "twelve", "thirteen", 
+                            "fourteen", "fifteen", "sixteen", "seventeen", 
+                            "eighteen", "nineteen", "twenty", "twenty one", 
+                            "twenty two", "twenty three", "twenty four", 
+                            "twenty five", "twenty six", "twenty seven", 
+                            "twenty eight", "twenty nine", 
+                        }; 
+        if(m==0)
         {
-            if(M > 1)
+            s=nums[h]+" o' clock";
+        }
+
+        else if(m>=1&&m<=30)
+        {
+            if(m==15)
             {
-                if(M > 20)
-                {System.out.print("twenty "+words.get(M-20)+" minutes past "+words.get(H));}
-                else if(M == 15)
-                {System.out.print("quarter past "+words.get(H));}
-                else
-                {System.out.print(words.get(M)+" minutes past "+words.get(H));}
+                s="quarter past "+nums[h];
+
             }
-            else
-            {System.out.print(words.get(M)+" minute past "+words.get((H+1)%12));}
+
+            else if(m==30)
+            {
+                s="half past "+nums[h];
+            }
+
+            else if(m==1){
+                s=nums[m]+" minute past "+nums[h];
+            }
+
+            else{
+                s=nums[m]+" minutes past "+nums[h];
+            }
         }
-        else if(M > 30)
+
+        else if(m>30)
         {
-            if(M==45)
-            {System.out.print("quarter to "+words.get((H+1)%12));}
-            else if(60-M > 20)
-            {System.out.print("twenty "+words.get(60-M-20)+" minutes to "+words.get((H+1)%12));}
-            else if(60-M < 20){System.out.print(words.get(60-M)+" minutes to "+words.get((H+1)%12));}
-            else{System.out.println("twenty minutes to "+words.get((H+1)%12));}
+            if(m==45)
+            {
+                s="quarter to "+nums[h+1];
+            }
+
+            else if(m==59)
+            {
+                s="one minute to "+nums[h+1];
+            }
+
+            else{
+                s=nums[60-m]+" minutes to "+nums[h+1];
+            }
         }
-        else
-        {System.out.print("half past "+words.get(H));}
-        
+        return s;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int h = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = timeInWords(h, m);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
