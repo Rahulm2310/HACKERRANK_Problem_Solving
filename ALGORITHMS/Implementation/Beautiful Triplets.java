62a3269941565b0f1a6224876cf866e02a796a25
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-11 17:01:46 +0530

    Update Beautiful Triplets.java

--- ALGORITHMS/Implementation/Beautiful Triplets.java
+++ ALGORITHMS/Implementation/Beautiful Triplets.java
@@ -1,41 +1,66 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        int d = input.nextInt();
-        
-        Set<Integer>  values = new HashSet<>();
-        
-        int beautifulTriplets = 0;
-        
-        
-        //Build a set
-        for(int i = 0; i < n; i++)
+    
+    static int beautifulTriplets(int d, int[] arr) {
+        int count=0;
+        for(int i=0;i<arr.length;i++)
         {
-            int x = input.nextInt();
-            
-            if(!values.contains(x))
+
+            for(int j=i+1;j<arr.length;j++)
             {
-                values.add(x);
+                if(arr[j]-arr[i]==d)
+                {
+                    for(int k=j+1;k<arr.length;k++)
+                    {
+                        if(arr[k]-arr[j]==d)
+                        {
+                            count++;
+                        }
+                    }
+                }
             }
         }
-        
-        
-        
-        //Check if set has a value, value+d, and value + 2d
-        for(Integer digit : values)
-        {
-            if(values.contains(digit+d) && values.contains(digit+(2*d)))
-            {
-                beautifulTriplets++;
-            }
+
+        return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
         }
-        
-        System.out.println(beautifulTriplets);
+
+        int result = beautifulTriplets(d, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
