70734bc999535252025328d95663dd832349f8a5
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-11 17:08:02 +0530

    Update Library Fine.java

--- ALGORITHMS/Implementation/Library Fine.java
+++ ALGORITHMS/Implementation/Library Fine.java
@@ -1,12 +1,11 @@
-//Problem: https://www.hackerrank.com/challenges/find-digits
-//Java 8
+
 import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       
         Scanner input = new Scanner(System.in);
         int T = input.nextInt();
         
