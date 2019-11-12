b15ceb1319b62f07f5764e0516385cadfbea311d
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-11-12 18:36:08 +0530

    Update Sherlock & Squares.java

--- ALGORITHMS/Implementation/Sherlock & Squares.java
+++ ALGORITHMS/Implementation/Sherlock & Squares.java
@@ -1,23 +1,25 @@
-//Problem: https://www.hackerrank.com/challenges/find-digits
-//Java 8
+
 import java.io.*;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+      
         Scanner input = new Scanner(System.in);
         int T = input.nextInt();
         
         for(int i = 0; i < T; i++)
         {
+            
             String numberString = String.valueOf(input.nextInt());
             int numberInteger = Integer.parseInt(numberString);
             int evenlyDivisibleDigits = 0;
             
+            
             for(int j = 0; j < numberString.length(); j++)
             {
+                
                 int digit = Character.getNumericValue(numberString.charAt(j));
                 
                 if(digit != 0 && numberInteger % digit == 0)
@@ -25,6 +27,7 @@ public class Solution {
                     evenlyDivisibleDigits++;
                 }
             }
+            
             System.out.println(evenlyDivisibleDigits);
         }
     }
