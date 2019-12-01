7f8ffbec7da6e12509ddee464dc5956f4feae33b
Rahul Mohata <54268438+Rahulm2310@users.noreply.github.com>
2019-12-01 23:18:16 +0530

    Update Bitwise AND.java

--- ALGORITHMS/Implementation/Bitwise AND.java
+++ ALGORITHMS/Implementation/Bitwise AND.java
@@ -1,19 +1,34 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
 
-    public static void main(String[] args) {
+public class Solution
+{
+
+    public static void main(String[] args) 
+    {
+        
         Scanner input = new Scanner(System.in);
         int T = input.nextInt();
+        
+        
         for(int t = 0; t < T; t++)
         {
+            
             int max = 0;
             int n = input.nextInt();
             int k = input.nextInt();
-            if (((k-1)|k) <= n) System.out.println(k-1);
-            else System.out.println(k-2);
+            
+            
+            if (((k-1)|k) <= n) 
+                System.out.println(k-1);
+            
+            else
+                System.out.println(k-2);
+            
             
         }
+        
     }
+    
 }
